54cdbc007e0e0f3f329b5c1af779921fde195034
Geyzha <Geyzhakey@gmail.com>
2024-07-31 11:25:19 +0800

    selesai tugas

--- kode1.cpp
+++ kode1.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 using namespace std;
 
+void cetakdata(string nama,string nisn, string jurusan,int TinggiBadan){}
+
 int main() {
+    int TinggiBadan= 100000 ;
+    string nama="Geyzha", nisn="86240";
+    string jurusan="PPLG";
+    
+    cout << "masukkan nama anda : " << nama << endl;
+
+    cout << "masukkan nisn anda : " << nisn << endl;
+
+    cout << "apa jurusan anda : " << jurusan << endl;
+
+    cout << "berapa tinggi badanmu : " << TinggiBadan << endl;
+
+    cetakdata(nama,nisn,jurusan,TinggiBadan);
+
     cout << "Selamat Datang di SMK TI Airlangga" << endl;
+    
     return 0;
 }
